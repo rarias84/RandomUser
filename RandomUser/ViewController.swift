35a6451c506b4f6f8047ce2d2e7adb3e63e13bfe
Reinaldo Arias <reinaldoarias.ext@tanner.cl>
2020-04-30 11:43:55 -0400

    init project

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by Reinaldo Arias on 30-04-20.
+//  Copyright © 2020 Reinaldo Arias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
